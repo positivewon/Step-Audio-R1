2f8e4615e4693fc7b3d120a2f66890961bd3c1c9
i-zhangyuxin02 <i-zhangyuxin02@stepfun.com>
2025-11-19 23:33:28 +0800

    添加benchmark例子

--- examples-vllm_r1.py
+++ examples-vllm_r1.py
@@ -6,17 +6,121 @@ from stepaudior1vllm import StepAudioR1
 # Audio understanding
 def mmau_test(model):
     messages = [
-        {"role": "system", "content": "You are an expert in audio analysis, please analyze the audio content and answer the questions accurately."},
         {"role": "human", "content": [
             {"type": "audio", "audio": "assets/mmau_test.wav"},
-            {"type": "text", "text": f"Which of the following best describes the male vocal in the audio? Please choose the answer from the following options: [Soft and melodic, Aggressive and talking, High-pitched and singing, Whispering] Output the final answer in <RESPONSE> </RESPONSE>."}
+            {"type": "text", "text": f"Which of the following best describes the male vocal in the audio? Please choose the answer from the following options: [Soft and melodic, Aggressive and talking, High-pitched and singing, Whispering]"}
         ]},
         {"role": "assistant", "content": "<think>\n", "eot": False},
     ]
 
     full_text = ""
     try:
-        for response, text, audio in model.stream(messages, max_tokens=1024, temperature=0.7, repetition_penalty=1.07, stop_token_ids=[151665]):
+        for response, text, audio in model.stream(messages, max_tokens=32000, temperature=0.7, repetition_penalty=1.07, stop_token_ids=[151665]):
+            if text:
+                full_text += text
+    except Exception as e:
+        print(f"Error during streaming: {e}")
+        import traceback
+        traceback.print_exc()
+
+    print("\n\nFull response:", full_text)
+
+def mmsu_test(model):
+    messages = [
+        {"role": "human", "content": [
+            {"type": "audio", "audio": "assets/mmsu_test.wav"},
+            {"type": "text", "text": f"What type of non-verbal sound is in the audio? Please choose the answer from the following options: [laugh, burp, cough, yawn]"}
+        ]},
+        {"role": "assistant", "content": "<think>\n", "eot": False},
+    ]
+
+    full_text = ""
+    try:
+        for response, text, audio in model.stream(messages, max_tokens=32000, temperature=0.7, repetition_penalty=1.07, stop_token_ids=[151665]):
+            if text:
+                full_text += text
+    except Exception as e:
+        print(f"Error during streaming: {e}")
+        import traceback
+        traceback.print_exc()
+
+    print("\n\nFull response:", full_text)
+
+# math audio question answering
+def spoken_mqa_test(model):
+    messages = [
+        {"role": "human", "content": [
+            {"type": "audio", "audio": "assets/spoken_mqa_test.wav"},
+            {"type": "text", "text": f"Solve the given math question step by step."}
+        ]},
+        {"role": "assistant", "content": "<think>\n", "eot": False},
+    ]
+
+    full_text = ""
+    try:
+        for response, text, audio in model.stream(messages, max_tokens=32000, temperature=0.7, repetition_penalty=1.07, stop_token_ids=[151665]):
+            if text:
+                full_text += text
+    except Exception as e:
+        print(f"Error during streaming: {e}")
+        import traceback
+        traceback.print_exc()
+
+    print("\n\nFull response:", full_text)
+
+def big_bench_audio_test(model):
+    messages = [
+        {"role": "human", "content": [
+            {"type": "audio", "audio": "assets/big_bench_audio_test.wav"},
+            {"type": "text", "text": '仅用 "valid" 或 "invalid"，"yes" 或 "no"，或者数字来直接回答上面的问题，不要添加任何其他描述。'}
+        ]},
+        {"role": "assistant", "content": "<think>\n", "eot": False},
+    ]
+
+    full_text = ""
+    try:
+        for response, text, audio in model.stream(messages, max_tokens=32000, temperature=0.7, repetition_penalty=1.07, stop_token_ids=[151665]):
+            if text:
+                full_text += text
+    except Exception as e:
+        print(f"Error during streaming: {e}")
+        import traceback
+        traceback.print_exc()
+
+    print("\n\nFull response:", full_text)
+
+def mmar_test(model):
+    messages = [
+        {"role": "human", "content": [
+            {"type": "audio", "audio": "assets/mmar_test.wav"},
+            {"type": "text", "text": f"Is the first little girl sincerely praising the other for being kind? Please choose the answer from the following options, do not provide any additional explanations or content: [Yes, No]"}
+        ]},
+        {"role": "assistant", "content": "<think>\n", "eot": False},
+    ]
+
+    full_text = ""
+    try:
+        for response, text, audio in model.stream(messages, max_tokens=32000, temperature=0.7, repetition_penalty=1.07, stop_token_ids=[151665]):
+            if text:
+                full_text += text
+    except Exception as e:
+        print(f"Error during streaming: {e}")
+        import traceback
+        traceback.print_exc()
+
+    print("\n\nFull response:", full_text)
+
+def wild_speech_test(model):
+    messages = [
+        {"role": "human", "content": [
+            {"type": "audio", "audio": "assets/wild_speech_test.wav"}
+        ]},
+        {"role": "assistant", "content": "<think>\n", "eot": False},
+    ]
+
+    full_text = ""
+    try:
+        for response, text, audio in model.stream(messages, max_tokens=32000, temperature=0.7, repetition_penalty=1.07, stop_token_ids=[151665]):
             if text:
                 full_text += text
     except Exception as e:
@@ -87,15 +191,21 @@ def Speaker_Trait_Inference(model):
 
 if __name__ == '__main__':
     # 修改为新的API URL和模型名称
-    api_url = "http://localhost:9999/v1/chat/completions"
+    api_url = "http://100.99.28.120:9999/v1/chat/completions"
     model_name = "step-audio-2-r1"
     
     model = StepAudioR1(api_url, model_name)
     
     
-    mmau_test(model)
+    
     song_appreciation(model)
     Speaker_Trait_Inference(model)
     uac_test(model)
+    spoken_mqa_test(model)
+    mmau_test(model)
+    mmsu_test(model)
+    big_bench_audio_test(model)
+    mmar_test(model)
+    wild_speech_test(model)
     
    
